4abcab965f69147e1ad0f793ddc279c5fbe0ebeb
misung <voielactee7@naver.com>
2023-08-31 18:04:25 +0900

    :sparkles: main 화면 띄우기

--- src/code/Main.java
+++ src/code/Main.java
@@ -2,9 +2,12 @@ package code;
 
 public class Main {
 
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	
 	public static void main(String[] args) {
-
 		
+		new START();
 		
 	}
 
